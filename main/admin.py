017968bc7c50cb037e92cac921e38154e7e44e01
zhenerBY <zhener@tut.by>
2021-11-25 18:17:27 +0300

    modify admin page

--- main/admin.py
+++ main/admin.py
@@ -2,28 +2,28 @@ from django.contrib import admin
 
 from .models import AdvUser, Categories, Operations
 
-# class BbAdmin(admin.ModelAdmin) :
-#     list_display = (’title’, ’content’, ’price’, ’published’)
-#     list_display_links = (’title’, ’content’)
-#     search_fields = (’title’, ’content’, )
-# # admin.site.register(Bb, BbAdmin)
 
 class CategoriesAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'expense', 'income', 'created_at')
     list_display_links = ('name',)
     search_fields = ('name',)
     ordering = ('name',)
-    # radio_fields = {'operation': admin.VERTICAL}
-
-    # name = models.CharField(max_length=50,
-    #                         db_index=True,
-    #                         unique=True,
-    #                         verbose_name='Нименование категории')
-    # expense = models.BooleanField(default=None, null=True, blank=True, db_index=True, verbose_name='Расход?')
-    # income = models.BooleanField(default=None, null=True, blank=True, db_index=True, verbose_name='Доход?')
-    # created_at
 
 
-admin.site.register(AdvUser)
+class AdvUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'first_name', 'last_name', 'is_superuser', 'is_active')
+    list_display_links = ('username', 'first_name', 'last_name',)
+    search_fields = ('username', 'first_name', 'last_name', 'email',)
+    ordering = ('username',)
+
+
+class OperationsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'amount', 'user', 'category',)
+    list_display_links = ('title', 'user',)
+    search_fields = ('title', 'description',)
+    ordering = ('-created_at',)
+
+
+admin.site.register(AdvUser, AdvUserAdmin)
 admin.site.register(Categories, CategoriesAdmin)
-admin.site.register(Operations)
+admin.site.register(Operations, OperationsAdmin)
